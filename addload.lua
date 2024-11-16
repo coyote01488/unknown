4807f616f5fbde94bdcbce39aa72931e414a246c
coyote01488 <166956344+coyote01488@users.noreply.github.com>
2024-11-17 00:59:49 +0100

    Update addload.lua

--- addload.lua
+++ addload.lua
@@ -1,10 +1,17 @@
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
-print("ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА")
+local Screen = Instance.new("ScreenGui")
+local Label = Instance.new("TextLabel")
+
+Screen.Name = "s9HSkVv1l1xazfli"
+Screen.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Screen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Screen.ResetOnSpawn = false
+
+Label.Name = "GKNsknTIKASNETLDASm"
+Label.Parent = Screen
+Label.BackgroundTransparency = 1
+Label.Position = UDim2.new(0, 0, 0, 0)
+Label.Size = UDim2.new(1, 0, 1, 0)
+Label.Text = "ЧЛЕН МИШИ БЫЛ В МАТЕРИ ДАМИРА"
+Label.TextColor3 = Color3.fromRGB(255, 0, 0)
+Label.TextScaled = true
+Label.TextSize = 32
