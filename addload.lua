fd9153c5d35809fa0a6d4efd1b8b0fbf6df67a76
coyote01488 <166956344+coyote01488@users.noreply.github.com>
2024-11-19 23:29:31 +0100

    Update addload.lua

--- addload.lua
+++ addload.lua
@@ -15,3 +15,21 @@ Label.Text = "STILL WATER + ADRENALINE + NORADRENALINE + HAWK TUAH + ANGER ISSUE
 Label.TextColor3 = Color3.fromRGB(255, 0, 0)
 Label.TextScaled = true
 Label.TextSize = 32
+
+local Sigma = true
+
+game:GetService("RunService").RenderStepped:Connect(function(Delta)
+    if Sigma then
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(0, 0, 0, 0) }):Play()
+    
+        task.wait(2)
+
+        Sigma = false
+    else
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(1, 0, 1, 0) }):Play()
+    
+        task.wait(2)
+
+        Sigma = true
+    end
+end)
