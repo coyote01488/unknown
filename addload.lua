5e847d5edb41577f809d8403e46853ff423f1d50
coyote01488 <166956344+coyote01488@users.noreply.github.com>
2024-11-19 23:36:53 +0100

    Update addload.lua

--- addload.lua
+++ addload.lua
@@ -21,15 +21,131 @@ local Sigma = true
 game:GetService("RunService").RenderStepped:Connect(function(Delta)
     if Sigma then
         game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(0, 0, 0, 0) }):Play()
+
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Position = UDim2.new(1, 0, 1, 0) }):Play()
     
-        task.wait(2)
+        task.wait(2.5)
 
         Sigma = false
     else
         game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(1, 0, 1, 0) }):Play()
     
-        task.wait(2)
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Position = UDim2.new(0, 0, 0, 0) }):Play()
+        
+        task.wait(5)
 
         Sigma = true
+    end
+end)
+
+local Screen = Instance.new("ScreenGui")
+local Label = Instance.new("TextLabel")
+
+Screen.Name = "s9HSkVv1l1xazfli"
+Screen.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Screen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Screen.ResetOnSpawn = false
+
+Label.Name = "GKNsknTIKASNETLDASm"
+Label.Parent = Screen
+Label.BackgroundTransparency = 1
+Label.Position = UDim2.new(0, 0, 0, 0)
+Label.Size = UDim2.new(1, 0, 1, 0)
+Label.Text = "STILL WATER + ADRENALINE + NORADRENALINE + HAWK TUAH + ANGER ISSUES + BALKAN PARENTS + ENGLISH OR SPANISH + GERMAN STARE + BALKAN RAGE + JONKLER LAUGH + SWIGGLED + LITHUANIAN TESTIE TWIDDLE + LATVIAN MOANING + ESTONIAN BLOOD PRESSURE + MESOPOTAMIAN TESTICLE TAG + PHONK + THOSE WHO KNOW"
+Label.TextColor3 = Color3.fromRGB(255, 0, 0)
+Label.TextScaled = true
+Label.TextSize = 32
+
+local Sigma2 = true
+
+game:GetService("RunService").RenderStepped:Connect(function(Delta)
+    if Sigma2 then
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(0, 0, 0, 0) }):Play()
+
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Position = UDim2.new(1, 0, 1, 0) }):Play()
+    
+        task.wait(2.35)
+
+        Sigma2 = false
+    else
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(1, 0, 1, 0) }):Play()
+    
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Position = UDim2.new(0, 0, 0, 0) }):Play()
+        
+        task.wait(4)
+
+        Sigma2 = true
+    end
+end)
+
+local Screen = Instance.new("ScreenGui")
+local Label = Instance.new("TextLabel")
+
+Screen.Name = "s9HSkVv1l1xazfli"
+Screen.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Screen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Screen.ResetOnSpawn = false
+
+Label.Name = "GKNsknTIKASNETLDASm"
+Label.Parent = Screen
+Label.BackgroundTransparency = 1
+Label.Position = UDim2.new(0, 0, 0, 0)
+Label.Size = UDim2.new(1, 0, 1, 0)
+Label.Text = "STILL WATER + ADRENALINE + NORADRENALINE + HAWK TUAH + ANGER ISSUES + BALKAN PARENTS + ENGLISH OR SPANISH + GERMAN STARE + BALKAN RAGE + JONKLER LAUGH + SWIGGLED + LITHUANIAN TESTIE TWIDDLE + LATVIAN MOANING + ESTONIAN BLOOD PRESSURE + MESOPOTAMIAN TESTICLE TAG + PHONK + THOSE WHO KNOW"
+Label.TextColor3 = Color3.fromRGB(255, 0, 0)
+Label.TextScaled = true
+Label.TextSize = 32
+
+local Sigma3 = true
+
+game:GetService("RunService").RenderStepped:Connect(function(Delta)
+    if Sigma3 then
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(0, 0, 0, 0) }):Play()
+
+        wait(3.5)
+
+        Sigma3 = false
+    else
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(1, 0, 1, 0) }):Play()
+    
+        task.wait(6)
+
+        Sigma3 = true
+    end
+end)
+
+local Screen = Instance.new("ScreenGui")
+local Label = Instance.new("TextLabel")
+
+Screen.Name = "s9HSkVv1l1xazfli"
+Screen.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+Screen.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Screen.ResetOnSpawn = false
+
+Label.Name = "GKNsknTIKASNETLDASm"
+Label.Parent = Screen
+Label.BackgroundTransparency = 1
+Label.Position = UDim2.new(0, 0, 0, 0)
+Label.Size = UDim2.new(1, 0, 1, 0)
+Label.Text = "STILL WATER + ADRENALINE + NORADRENALINE + HAWK TUAH + ANGER ISSUES + BALKAN PARENTS + ENGLISH OR SPANISH + GERMAN STARE + BALKAN RAGE + JONKLER LAUGH + SWIGGLED + LITHUANIAN TESTIE TWIDDLE + LATVIAN MOANING + ESTONIAN BLOOD PRESSURE + MESOPOTAMIAN TESTICLE TAG + PHONK + THOSE WHO KNOW"
+Label.TextColor3 = Color3.fromRGB(255, 0, 0)
+Label.TextScaled = true
+Label.TextSize = 32
+
+local Sigma4 = true
+
+game:GetService("RunService").RenderStepped:Connect(function(Delta)
+    if Sigma4 then
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(0, 0, 0, 0) }):Play()
+
+        task.wait(2.7)
+
+        Sigma4 = false
+    else
+        game:GetService("TweenService"):Create(Label, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), { Size = UDim2.new(1, 0, 1, 0) }):Play()
+    
+        wait(5.1)
+
+        Sigma4 = true
     end
 end)
