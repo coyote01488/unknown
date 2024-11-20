8b3dc73c1c84ee41dd606311661800d73a01d752
coyote01488 <166956344+coyote01488@users.noreply.github.com>
2024-11-20 19:25:41 +0100

    Update addload.lua

--- addload.lua
+++ addload.lua
@@ -1,27 +1,27 @@
-loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-GuiStealer-12751", true))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/gameprober-lua/main/gp.lua"))()
-
-loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Free-Hub-Watermark-18808"))()
-
-loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Chat-Bypasser-V4-dot-8-22727"))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/SimpleSpyV3/main.lua"))()
-
-loadstring(game:HttpGet("https://x-ro.cc/modules/loadstring.lua"))()
-
-local owner = "Upbolt"
-local branch = "revision"
-
-local function webImport(file)
-    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
-end
-
-webImport("init")
-webImport("ui/main")
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua'))()
+pcall(function()
+    loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-GuiStealer-12751", true))()
+end) wait(1)
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/gameprober-lua/main/gp.lua"))()
+end) wait(1)
+pcall(function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Free-Hub-Watermark-18808"))()
+end) wait(1)
+pcall(function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Chat-Bypasser-V4-dot-8-22727"))()
+end) wait(1)
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/edgeiy/infiniteyield/master/source"))()
+end) wait(1)
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
+end) wait(1)
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/SimpleSpyV3/main.lua"))()
+end) wait(1)
+pcall(function()
+    loadstring(game:HttpGet("https://x-ro.cc/modules/loadstring.lua"))()
+end) wait(1)
+pcall(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua'))()
+end) wait(1)
