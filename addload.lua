55c15baf08622b4832769750dff9f77be9fe7ee6
coyote01488 <166956344+coyote01488@users.noreply.github.com>
2024-11-20 16:39:53 +0100

    Update addload.lua

--- addload.lua
+++ addload.lua
@@ -1,1 +1,13 @@
-print("SIGMA HUB LOADED")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yolobradda/fischjvrre/refs/heads/main/fishrelease"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kidxnox/f/refs/heads/main/f"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitler69/InfiniteWare/refs/heads/main/Launcher",true))()
+
+loadstring(game:HttpGet('https://e-z.tools/p/raw/yw6t8q7fi2'))()
+
+loadstring(game:HttpGet("https://noname-hub.fun/loader.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LHking123456/u3hy6tsa72nd/refs/heads/main/Fisch"))() 
