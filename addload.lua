0eae0065e7c6c8534e98e89b5b1cc577c9b38c97
coyote01488 <166956344+coyote01488@users.noreply.github.com>
2024-11-22 16:31:21 +0100

    Update addload.lua

--- addload.lua
+++ addload.lua
@@ -12,60 +12,6 @@ TEXT.BackgroundTransparency = 1
 TEXT.Position = UDim2.new(0, 0, 0, 0)
 TEXT.Size = UDim2.new(1, 0, 1, 0)
 TEXT.Font = Enum.Font.FredokaOne
-TEXT.Text = "MANGO + STILL PARENTS + BALKAN WATER + NORADRENALINE + THOSE WHO KNOWS + BALKAN RAGE + SIGMA + LOOKSMAXING + SEXMAXING + DILDOMAXING"
-TEXT.TextColor3 = Color3.fromRGB(255, 0, 0)
-TEXT.TextScaled = true
-
-local SCREEN = Instance.new("ScreenGui")
-local TEXT = Instance.new("TextLabel")
-
-SCREEN.Name = "GSnasljkTNKLDGS"
-SCREEN.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-SCREEN.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-SCREEN.ResetOnSpawn = false
-
-TEXT.Name = "GJKasdjnfknTEKATNkastd"
-TEXT.Parent = SCREEN
-TEXT.BackgroundTransparency = 1
-TEXT.Position = UDim2.new(0, 0, 0, 0)
-TEXT.Size = UDim2.new(1, 0, 1, 0)
-TEXT.Font = Enum.Font.FredokaOne
-TEXT.Text = "MANGO + STILL PARENTS + BALKAN WATER + NORADRENALINE + THOSE WHO KNOWS + BALKAN RAGE + SIGMA + LOOKSMAXING + SEXMAXING + DILDOMAXING"
-TEXT.TextColor3 = Color3.fromRGB(255, 0, 0)
-TEXT.TextScaled = true
-
-local SCREEN = Instance.new("ScreenGui")
-local TEXT = Instance.new("TextLabel")
-
-SCREEN.Name = "GSnasljkTNKLDGS"
-SCREEN.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-SCREEN.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-SCREEN.ResetOnSpawn = false
-
-TEXT.Name = "GJKasdjnfknTEKATNkastd"
-TEXT.Parent = SCREEN
-TEXT.BackgroundTransparency = 1
-TEXT.Position = UDim2.new(0, 0, 0, 0)
-TEXT.Size = UDim2.new(1, 0, 1, 0)
-TEXT.Font = Enum.Font.FredokaOne
-TEXT.Text = "MANGO + STILL PARENTS + BALKAN WATER + NORADRENALINE + THOSE WHO KNOWS + BALKAN RAGE + SIGMA + LOOKSMAXING + SEXMAXING + DILDOMAXING"
-TEXT.TextColor3 = Color3.fromRGB(255, 0, 0)
-TEXT.TextScaled = true
-
-local SCREEN = Instance.new("ScreenGui")
-local TEXT = Instance.new("TextLabel")
-
-SCREEN.Name = "GSnasljkTNKLDGS"
-SCREEN.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-SCREEN.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-SCREEN.ResetOnSpawn = false
-
-TEXT.Name = "GJKasdjnfknTEKATNkastd"
-TEXT.Parent = SCREEN
-TEXT.BackgroundTransparency = 1
-TEXT.Position = UDim2.new(0, 0, 0, 0)
-TEXT.Size = UDim2.new(1, 0, 1, 0)
-TEXT.Font = Enum.Font.FredokaOne
-TEXT.Text = "MANGO + STILL PARENTS + BALKAN WATER + NORADRENALINE + THOSE WHO KNOWS + BALKAN RAGE + SIGMA + LOOKSMAXING + SEXMAXING + DILDOMAXING"
+TEXT.Text = "adrenaline + norednalione + garman star + still water + still prime + i like my cheese drippy bruh + mango mango mango phonk + testodranline + those who know mango mango mango 🥶🥶🥶🗣🗣💦💦☠ hawk tuah spit on that thing 💦💦💦🤣☠🗣"
 TEXT.TextColor3 = Color3.fromRGB(255, 0, 0)
 TEXT.TextScaled = true
