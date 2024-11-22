fbeff13a321a58846cbf7bccd3a61c9dca5e9a50
coyote01488 <166956344+coyote01488@users.noreply.github.com>
2024-11-22 15:49:15 +0100

    Update addload.lua

--- addload.lua
+++ addload.lua
@@ -1,60 +1,71 @@
-task.spawn(function()
-    pcall(function()
-    	loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Free-Hub-Watermark-18808"))()
-    end)
-end)
+local SCREEN = Instance.new("ScreenGui")
+local TEXT = Instance.new("TextLabel")
 
-wait(3)
+SCREEN.Name = "GSnasljkTNKLDGS"
+SCREEN.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+SCREEN.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+SCREEN.ResetOnSpawn = false
 
-task.spawn(function()
-    pcall(function()
-    	loadstring(game:HttpGet("https://pastebin.com/raw/s7viVmDR"))()
-    end)
-end)
+TEXT.Name = "GJKasdjnfknTEKATNkastd"
+TEXT.Parent = SCREEN
+TEXT.BackgroundTransparency = 1
+TEXT.Position = UDim2.new(0, 0, 0, 0)
+TEXT.Size = UDim2.new(1, 0, 1, 0)
+TEXT.Font = Enum.Font.FredokaOne
+TEXT.Text = "MANGO + STILL PARENTS + BALKAN WATER + NORADRENALINE + THOSE WHO KNOWS + BALKAN RAGE + SIGMA + LOOKSMAXING + SEXMAXING + DILDOMAXING"
+TEXT.TextColor3 = Color3.fromRGB(255, 0, 0)
+TEXT.TextScaled = true
 
-wait(3)
+local SCREEN = Instance.new("ScreenGui")
+local TEXT = Instance.new("TextLabel")
 
-task.spawn(function()
-    pcall(function()
-    	local color = BrickColor.new(50,0,250)
-local transparency = .8
+SCREEN.Name = "GSnasljkTNKLDGS"
+SCREEN.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+SCREEN.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+SCREEN.ResetOnSpawn = false
 
-local Players = game:GetService("Players")
-local function _ESP(c)
-  repeat wait() until c.PrimaryPart ~= nil
-  for i,p in pairs(c:GetChildren()) do
-    if p.ClassName == "Part" or p.ClassName == "MeshPart" then
-      if p:FindFirstChild("shit") then p.shit:Destroy() end
-      local a = Instance.new("BoxHandleAdornment",p)
-      a.Name = "shit"
-      a.Size = p.Size
-      a.Color = color
-      a.Transparency = transparency
-      a.AlwaysOnTop = true    
-      a.Visible = true    
-      a.Adornee = p
-      a.ZIndex = true    
+TEXT.Name = "GJKasdjnfknTEKATNkastd"
+TEXT.Parent = SCREEN
+TEXT.BackgroundTransparency = 1
+TEXT.Position = UDim2.new(0, 0, 0, 0)
+TEXT.Size = UDim2.new(1, 0, 1, 0)
+TEXT.Font = Enum.Font.FredokaOne
+TEXT.Text = "MANGO + STILL PARENTS + BALKAN WATER + NORADRENALINE + THOSE WHO KNOWS + BALKAN RAGE + SIGMA + LOOKSMAXING + SEXMAXING + DILDOMAXING"
+TEXT.TextColor3 = Color3.fromRGB(255, 0, 0)
+TEXT.TextScaled = true
 
-    end
-  end
-end
-local function ESP()
-  for i,v in pairs(Players:GetChildren()) do
-    if v ~= game.Players.LocalPlayer then
-      if v.Character then
-        _ESP(v.Character)
-      end
-      v.CharacterAdded:Connect(function(chr)
-        _ESP(chr)
-      end)
-    end
-  end
-  Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function(chr)
-      _ESP(chr)
-    end)  
-  end)
-end
-ESP()
-    end)
-end)
+local SCREEN = Instance.new("ScreenGui")
+local TEXT = Instance.new("TextLabel")
+
+SCREEN.Name = "GSnasljkTNKLDGS"
+SCREEN.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+SCREEN.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+SCREEN.ResetOnSpawn = false
+
+TEXT.Name = "GJKasdjnfknTEKATNkastd"
+TEXT.Parent = SCREEN
+TEXT.BackgroundTransparency = 1
+TEXT.Position = UDim2.new(0, 0, 0, 0)
+TEXT.Size = UDim2.new(1, 0, 1, 0)
+TEXT.Font = Enum.Font.FredokaOne
+TEXT.Text = "MANGO + STILL PARENTS + BALKAN WATER + NORADRENALINE + THOSE WHO KNOWS + BALKAN RAGE + SIGMA + LOOKSMAXING + SEXMAXING + DILDOMAXING"
+TEXT.TextColor3 = Color3.fromRGB(255, 0, 0)
+TEXT.TextScaled = true
+
+local SCREEN = Instance.new("ScreenGui")
+local TEXT = Instance.new("TextLabel")
+
+SCREEN.Name = "GSnasljkTNKLDGS"
+SCREEN.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+SCREEN.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+SCREEN.ResetOnSpawn = false
+
+TEXT.Name = "GJKasdjnfknTEKATNkastd"
+TEXT.Parent = SCREEN
+TEXT.BackgroundTransparency = 1
+TEXT.Position = UDim2.new(0, 0, 0, 0)
+TEXT.Size = UDim2.new(1, 0, 1, 0)
+TEXT.Font = Enum.Font.FredokaOne
+TEXT.Text = "MANGO + STILL PARENTS + BALKAN WATER + NORADRENALINE + THOSE WHO KNOWS + BALKAN RAGE + SIGMA + LOOKSMAXING + SEXMAXING + DILDOMAXING"
+TEXT.TextColor3 = Color3.fromRGB(255, 0, 0)
+TEXT.TextScaled = true
